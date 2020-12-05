7cab725afec8bdca0c34e4bfeb8d36a2f964932b
Craig Miller <craigmiller160@gmail.com>
2020-12-05 15:20:05 -0500

    Finished end to end tests

--- test/execution.test.ts
+++ test/execution.test.ts
@@ -6,6 +6,51 @@ import execute from '../src/execution';
 
 const getCwdMock: Mock = getCwd as Mock;
 const doSpawnSyncMock: Mock = doSpawnSync as Mock;
+const jsCwd = path.resolve(process.cwd(), 'test/__data__/js');
+const mavenCwd = path.resolve(process.cwd(), 'test/__data__/js');
+const tag = 'localhost:32000/sample-project:1.0.0';
+
+const validateCommands = (cwd: string) => {
+    expect(doSpawnSyncMock).toHaveBeenNthCalledWith(1, {
+        command: 'sudo',
+        args: [
+            'docker',
+            'build',
+            '--network=host',
+            '-t',
+            tag,
+            '.'
+        ],
+        cwd: `${cwd}/deploy`
+    });
+    expect(doSpawnSyncMock).toHaveBeenNthCalledWith(2, {
+        command: 'sudo',
+        args: [
+            'docker',
+            'push',
+            tag
+        ],
+        cwd: `${cwd}/deploy`
+    });
+    expect(doSpawnSyncMock).toHaveBeenNthCalledWith(3, {
+        command: 'kubectl',
+        args: [
+            'apply',
+            '-f',
+            'configmap.yml'
+        ],
+        cwd: `${cwd}/deploy`
+    });
+    expect(doSpawnSyncMock).toHaveBeenNthCalledWith(4, {
+        command: 'kubectl',
+        args: [
+            'apply',
+            '-f',
+            'deployment.yml'
+        ],
+        cwd: `${cwd}/deploy`
+    });
+};
 
 describe('kube-deploy end-to-end', () => {
     beforeEach(() => {
@@ -13,14 +58,16 @@ describe('kube-deploy end-to-end', () => {
     });
 
     it('deploys JS project', () => {
-        getCwdMock.mockImplementation(() => path.resolve(process.cwd(), 'test/__data__/js'));
+        getCwdMock.mockImplementation(() => jsCwd);
         const status = execute();
         expect(status).toEqual(0);
+        validateCommands(jsCwd);
     });
 
     it('deploys Maven project', () => {
-        getCwdMock.mockImplementation(() => path.resolve(process.cwd(), 'test/__data__/maven'));
+        getCwdMock.mockImplementation(() => mavenCwd);
         const status = execute();
         expect(status).toEqual(0);
+        validateCommands(mavenCwd);
     });
 });
