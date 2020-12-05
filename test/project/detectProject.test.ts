2320eebf05cbf9d91a91fcf191db84619060ac7d
Craig <craigmiller160@gmail.com>
2020-12-05 09:34:10 -0500

    Implemented more tests

--- test/project/detectProject.test.ts
+++ test/project/detectProject.test.ts
@@ -1,19 +1,32 @@
 import path from 'path';
 import getCwd from '../../src/utils/getCwd';
 import Mock = jest.Mock;
+import detectProject, { ProjectType } from '../../src/project/detectProject';
 
 const getCwdMock = getCwd as Mock;
 
 describe('detectProject', () => {
     it('is JS project', () => {
-        throw new Error();
+        getCwdMock.mockImplementation(() => path.resolve(process.cwd(), 'test/__data__/js'));
+        const result: ProjectType = detectProject();
+        expect(result).toEqual(ProjectType.JavaScript);
     });
 
     it('is maven project', () => {
-        throw new Error();
+        getCwdMock.mockImplementation(() => path.resolve(process.cwd(), 'test/__data__/maven'));
+        const result: ProjectType = detectProject();
+        expect(result).toEqual(ProjectType.Maven);
     });
 
     it('is unknown project', () => {
-        throw new Error();
+        try {
+            getCwdMock.mockImplementation(() => path.resolve(process.cwd(), 'test/__data__'));
+            detectProject();
+        } catch (ex) {
+            expect(ex.message).toEqual('Cannot identify project type');
+            return;
+        }
+
+        throw new Error('Should have thrown error');
     });
 });
