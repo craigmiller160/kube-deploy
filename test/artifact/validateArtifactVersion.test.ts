fc3cdd80fd915b00989233b1bed3712f7fc03838
Craig <craigmiller160@gmail.com>
2020-12-05 09:48:37 -0500

    Stubbed out artifact version validating and tests

---
+++ test/artifact/validateArtifactVersion.test.ts
@@ -0,0 +1,11 @@
+import path from 'path';
+
+describe('validateArtifactVersion', () => {
+    it('version is valid', () => {
+        throw new Error();
+    });
+
+    it('version is not valid', () => {
+        throw new Error();
+    });
+});
