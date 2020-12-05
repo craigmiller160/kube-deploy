470adc2c40db5fddf37b7c6fe474f10264ca68b4
Craig <craigmiller160@gmail.com>
2020-12-05 13:05:08 -0500

    Wrote tests for the docker commands

--- test/commands/dockerCommands.test.ts
+++ test/commands/dockerCommands.test.ts
@@ -1,11 +1,41 @@
 import getCwd from '../../src/utils/getCwd';
+import { doSpawnSync } from '../../src/utils/doSpawn';
+import Mock = jest.Mock;
+import { dockerBuild, dockerPush } from '../../src/commands/dockerCommands';
+
+const getCwdMock: Mock = getCwd as Mock;
+const doSpawnSyncMock: Mock = doSpawnSync as Mock;
+
+const cwd = '/home/dir'
+const tag = 'localhost:3200/sample-project:1.0.0'
 
 describe('dockerCommands', () => {
+    beforeEach(() => {
+        getCwdMock.mockImplementation(() => cwd);
+    });
     it('dockerBuild', () => {
-        throw new Error();
+        dockerBuild(tag);
+        expect(doSpawnSyncMock).toHaveBeenLastCalledWith({
+            command: 'docker',
+            args: [
+                'build',
+                '--network=host',
+                `-t ${tag}`,
+                '.'
+            ],
+            cwd: `${cwd}/deploy`
+        });
     });
 
     it('dockerPush', () => {
-        throw new Error();
+        dockerPush(tag);
+        expect(doSpawnSyncMock).toHaveBeenLastCalledWith({
+            command: 'docker',
+            args: [
+                'push',
+                tag
+            ],
+            cwd: `${cwd}/deploy`
+        });
     });
 });
