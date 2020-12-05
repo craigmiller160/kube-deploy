4a90ce5add72096890db005cc6d635aa01872b97
Craig Miller <craigmiller160@gmail.com>
2020-12-05 15:11:47 -0500

    Setup end-to-end tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,23 @@
-import path from 'path';
+import getCwd from '../src/utils/getCwd';
+import { doSpawnSync } from '../src/utils/doSpawn';
+import Mock = jest.Mock;
+import * as path from 'path';
+
+const getCwdMock: Mock = getCwd as Mock;
+const doSpawnSyncMock: Mock = doSpawnSync as Mock;
 
 describe('kube-deploy end-to-end', () => {
-    it('create tests', () => {
+    beforeEach(() => {
+        doSpawnSyncMock.mockImplementation(() => ({ status: 0 }));
+    });
+
+    it('deploys JS project', () => {
+        getCwdMock.mockImplementation(() => path.resolve(process.cwd(), 'test/__data__/js'));
+        throw new Error();
+    });
+
+    it('deploys Maven project', () => {
+        getCwdMock.mockImplementation(() => path.resolve(process.cwd(), 'test/__data__/maven'));
         throw new Error();
     });
 });
