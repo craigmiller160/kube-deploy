31200f17c9b92ed9572076f129fa06b6b793898f
Craig <craigmiller160@gmail.com>
2020-12-05 09:10:32 -0500

    Working on the XML parsing logic

--- src/project/getProjectMaven.ts
+++ src/project/getProjectMaven.ts
@@ -1,5 +1,15 @@
+import path from 'path';
+import fs from 'fs';
+import { Parser } from 'xml2js';
 import ProjectInfo from '../types/ProjectInfo';
 
 export default (): ProjectInfo => {
-
+    const pomXml = fs.readFileSync(path.resolve(process.cwd(), 'pom.xml'), 'utf8');
+    const parser = new Parser();
+    const parsedPomXml = parser.parseString(pomXml);
+    console.log(parsedPomXml); // TODO finish this
+    return { // TODO fix this
+        name: '',
+        version: ''
+    };
 }
