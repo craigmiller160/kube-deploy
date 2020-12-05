0d3f0a30b8e84fb236097bfc13e6df46e1249090
Craig <craigmiller160@gmail.com>
2020-12-05 09:35:51 -0500

    Wrote more tests

---
+++ test/project/getProjectMaven.test.ts
@@ -0,0 +1,18 @@
+import path from 'path';
+import getCwd from '../../src/utils/getCwd';
+import Mock = jest.Mock;
+import ProjectInfo from '../../src/types/ProjectInfo';
+import getProjectMaven from '../../src/project/getProjectMaven';
+
+const getCwdMock = getCwd as Mock;
+
+describe('getProjectMaven', () => {
+    it('gets maven project info', () => {
+        getCwdMock.mockImplementation(() => path.resolve(process.cwd(), 'test/__data__/maven'));
+        const result: ProjectInfo = getProjectMaven();
+        expect(result).toEqual({
+            name: 'sample-project',
+            version: '1.0.0'
+        });
+    });
+});
