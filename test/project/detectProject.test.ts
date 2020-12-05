1998d9a567d0fdced13296263bde414dcd816ea2
Craig <craigmiller160@gmail.com>
2020-12-05 09:30:41 -0500

    Stubbed out more tests

---
+++ test/project/detectProject.test.ts
@@ -0,0 +1,15 @@
+import path from 'path';
+
+describe('detectProject', () => {
+    it('is JS project', () => {
+        throw new Error();
+    });
+
+    it('is maven project', () => {
+        throw new Error();
+    });
+
+    it('is unknown project', () => {
+        throw new Error();
+    });
+});
