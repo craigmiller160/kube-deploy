8989af9c362df0b0167b14e94d572f113e50f69b
Craig <craigmiller160@gmail.com>
2020-12-05 13:01:21 -0500

    Setup mocking of doSpawnSync

--- test/setup.ts
+++ test/setup.ts
@@ -1,11 +1,19 @@
 import getCwd from '../src/utils/getCwd';
+import { doSpawnSync } from '../src/utils/doSpawn';
 import Mock = jest.Mock;
 
 jest.mock('../src/utils/getCwd', () => {
     return jest.fn();
 });
 
+jest.mock('../src/utils/doSpawn', () => {
+    return {
+        doSpawnSync: jest.fn()
+    };
+});
+
 beforeEach(() => {
     jest.clearAllMocks();
     (getCwd as Mock).mockReset();
+    (doSpawnSync as Mock).mockReset();
 });
