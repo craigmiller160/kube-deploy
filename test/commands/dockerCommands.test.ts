d5c8a9cf4870680a1ccded4281b56a2fd9ab6a04
Craig Miller <craigmiller160@gmail.com>
2021-01-30 14:22:22 -0500

    Implemented docker login test

--- test/commands/dockerCommands.test.ts
+++ test/commands/dockerCommands.test.ts
@@ -1,22 +1,48 @@
 import getCwd from '../../src/utils/getCwd';
 import { doSpawnSync } from '../../src/utils/doSpawn';
 import Mock = jest.Mock;
-import { dockerBuild, dockerPush } from '../../src/commands/dockerCommands';
+import { dockerBuild, dockerLogin, dockerPush } from '../../src/commands/dockerCommands';
+import shellEnv from 'shell-env';
+import { DOCKER_REPO } from '../../src/utils/dockerConstants';
+
+jest.mock('shell-env', () => ({
+    sync: jest.fn()
+}));
 
 const getCwdMock: Mock = getCwd as Mock;
 const doSpawnSyncMock: Mock = doSpawnSync as Mock;
+const syncMock: Mock = shellEnv.sync as Mock;
 
 const cwd = '/home/dir';
-const tag = 'localhost:3200/sample-project:1.0.0';
+const tag = `${DOCKER_REPO}/sample-project:1.0.0`;
+const user = 'user';
+const password = 'password';
 
 describe('dockerCommands', () => {
     beforeEach(() => {
+        jest.resetAllMocks();
         getCwdMock.mockImplementation(() => cwd);
         doSpawnSyncMock.mockImplementation(() => ({ status: 0 }));
+        syncMock.mockImplementation(() => ({
+            NEXUS_DOCKER_USER: user,
+            NEXUS_DOCKER_PASSWORD: password
+        }));
     });
 
     it('dockerLogin', () => {
-        throw new Error();
+        dockerLogin();
+        expect(doSpawnSyncMock).toHaveBeenCalledWith({
+            command: 'sudo',
+            args: [
+                'docker',
+                'login',
+                DOCKER_REPO,
+                '-u',
+                user,
+                '-p',
+                password
+            ]
+        });
     });
 
     it('dockerBuild', () => {
